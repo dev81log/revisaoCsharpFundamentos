1e9e23e385b9011465e626ab8291591e0801000b
dev81log <dev81log@gmail.com>
2022-05-15 17:34:54 -0300

    Finalizando estudos fundamentos

--- Program.cs
+++ Program.cs
@@ -6,16 +6,6 @@ namespace MeuApp
     {
         static void Main(string[] args)
         {
-
-            Cotacao cotacao = new Cotacao(10, "Dolar Canadense", 5.14);
-            cotacao.Id = 15;
-            cotacao.Moeda = "Dolar Canadense!";
-            cotacao.Preco = 5.16;
-
-            Console.WriteLine(cotacao.Id);
-            Console.WriteLine(cotacao.Moeda);
-            Console.WriteLine(cotacao.Preco);
-
             var meuNome = RetornaNome("Asher", "Duarte");
             Console.WriteLine(meuNome);
 
@@ -48,6 +38,17 @@ namespace MeuApp
                 Console.WriteLine("ainda é menor");
                 nota++;
             }
+
+            Cotacao cotacao = new Cotacao(10, "Dolar Canadense", 5.14, ECompraVenda.Abertura);
+            cotacao.Id = 15;
+            cotacao.Moeda = "Dolar Canadense!";
+            cotacao.Preco = 5.16;
+
+
+            Console.WriteLine(cotacao.Id);
+            Console.WriteLine(cotacao.Moeda);
+            Console.WriteLine(cotacao.Preco);
+            Console.WriteLine((int)cotacao.CompraVenda);
         }
 
         static string RetornaNome(string nome, string sobrenome)
@@ -58,21 +59,29 @@ namespace MeuApp
 
     struct Cotacao
     {
-        public Cotacao(int id, string moeda, double preco)
+        public Cotacao(int id, string moeda, double preco, ECompraVenda compraVenda)
         {
             Id = id;
             Moeda = moeda;
             Preco = preco;
+            CompraVenda = compraVenda;
         }
 
         public int Id;
         public string Moeda;
         public double Preco;
+        public ECompraVenda CompraVenda;
 
         public double ConversorDolar(double dolar)
         {
             return dolar * Preco;
         }
+    }
+
+    enum ECompraVenda
+    {
+        Abertura = 1,
+        Fechamento = 2
     }
 }
 
